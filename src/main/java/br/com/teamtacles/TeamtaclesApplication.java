914d8dbaea383459154690f559b80efcc9b0041b
Pedro Lauton <lautonpedro@gmail.com>
2025-09-01 18:43:12 -0300

    Initial commit

---
+++ src/main/java/br/com/teamtacles/TeamtaclesApplication.java
@@ -0,0 +1,13 @@
+package br.com.teamtacles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtaclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamtaclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teamtacles/TeamtaclesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teamtacles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamtaclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
